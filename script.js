95610796af4adf528e9d2454d02d7412e3ad10ad
Maximilian <maxihuetter123@gmail.com>
2023-01-27 12:05:02 +0100

    v0.5
    customers and review slides added

--- script.js
+++ script.js
@@ -24,3 +24,31 @@ instagramlink.addEventListener("click", (e) => {
         localStorage.setItem('cookies', true)
         document.getElementById('cookie-disclaimer-overlay').style.display = ''
     })
+
+//customer reviews
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+}
