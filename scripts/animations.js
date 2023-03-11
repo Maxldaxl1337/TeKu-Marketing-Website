664736c21ba36796478fb83415c69f888ba9c6e5
Maximilian <maxihuetter123@gmail.com>
2023-03-11 12:09:18 +0100

    j

--- scripts/animations.js
+++ scripts/animations.js
@@ -49,7 +49,6 @@ observer.observe(document.querySelector('.mehrlesen'));
 
 observer.observe(document.querySelector(".review-slider"));
 observer.observe(document.querySelector(".headerkunden"));
-//observer.observe(document.querySelector(".headerunserekunden"));
 observer.observe(document.querySelector(".certificate-container"));
 observer.observe(document.querySelector(".borderslideup"));
 observer.observe(document.querySelector(".borderslideup1"));
